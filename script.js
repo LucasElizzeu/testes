1095fb723f4534a5051ac65495dbedf1a254908b
Lucas Elizeu <lucaselizzeu@gmail.com>
2025-03-16 12:26:33 -0300

    adicionado constructor functions criar celular

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function criarNotebook(marca, ram, memoria, processador) {
-    return {
-        marca,
-        ram,
-        memoria,
-        processador,
-        ligar() {
-            console.log('O notebook está ligado.');
-        }
+// function criarNotebook(marca, ram, memoria, processador) {
+//     return {
+//         marca,
+//         ram,
+//         memoria,
+//         processador,
+//         ligar() {
+//             console.log('O notebook está ligado.');
+//         }
+//     }
+// }
+
+// const notebook1 = criarNotebook('samsung', 16, 512, 'i5');
+// console.log(notebook1);
+
+function Celular(marca, ano, tamanhotela) {
+    this.marca = marca,
+        this.ano = ano,
+        this.tamanhotela = tamanhotela,
+        this.ligar = function() {
+        console.log('O celular está ligando...')
+
     }
 }
 
-const notebook1 = criarNotebook('samsung', 16, 512, 'i5');
-console.log(notebook1);
+const celular = new Celular('samsung', 2022, 6.1);
+console.log(celular);
